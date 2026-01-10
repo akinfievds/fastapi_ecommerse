30aff3c14cd6a32ba2e77a421c47114bd96b0dc9
Akinfiev Dmitrii <akinfievds@gmail.com>
2026-01-11 02:25:32 +0300

    Add Shopping Cart schemas

--- app/schemas.py
+++ app/schemas.py
@@ -157,3 +157,54 @@ class ProductList(BaseModel):
     page_size: int = Field(ge=1, description="Количество элементов на странице")
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class CartItemBase(BaseModel):
+    """
+    Базовая модель, которая содержит минимальный набор полей,
+    необходимых для идентификации товара и его количества в корзине
+    """
+
+    product_id: int = Field(description="ID корзины")
+    quantity: int = Field(ge=1, description="Количество товара")
+
+
+class CartItemCreate(CartItemBase):
+    """
+    Модель для добавления нового товара в корзину
+    """
+
+    pass
+
+
+class CartItemUpdate(BaseModel):
+    """
+    Модель для обновления количества товара в корзине
+    """
+
+    quantity: int = Field(..., ge=1, description="Новое количество товара")
+
+
+class CartItem(BaseModel):
+    """
+    Товар в корзине с данными продукта
+    """
+
+    id: int = Field(..., description="ID позиции корзины")
+    quantity: int = Field(..., ge=1, description="Количество товара")
+    product: Product = Field(..., description="Информация о товаре")
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Cart(BaseModel):
+    """
+    Полнаяи информация о корзине пользователя
+    """
+
+    user_id: int = Field(..., description="ID пользователя")
+    items: list[CartItem] = Field(default_factory=list, description="Содержимое корзины")
+    total_quantity: int = Field(..., ge=0, description="Общее количество товаров")
+    total_price: Decimal = Field(..., ge=0, description="Общая стоимость товаров")
+
+    model_config = ConfigDict(from_attributes=True)
