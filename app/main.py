50938f7c161c8bb2eaf91ab677fb3df4c5d703c0
Akinfiev Dmitrii <akinfievds@gmail.com>
2026-01-12 23:16:24 +0300

    Add logging with loguru

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from uuid import uuid4
 
-from app.routers import categories, cart, orders, products, reviews, users
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+from loguru import logger
+
+from app.routers import cart, categories, orders, products, reviews, users
+
+logger.add(
+    "info.log",
+    format="Log: [{extra[log_id]}:{time} {level} {message}]",
+    level="DEBUG",
+    enqueue=True,
+)
+
 
 # Создаём приложение FastAPI
 app = FastAPI(
@@ -20,6 +32,26 @@ app.include_router(orders.router)
 
 # Монтирование подприложения StaticFiles
 app.mount("/media", StaticFiles(directory="media"), name="media")
+
+
+# Middleware для регистрации сообщений
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info("Succesfully accessed " + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(
+                content={"success": False},
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+        return response
 
 
 # Корневой эндпоинт для проверки
